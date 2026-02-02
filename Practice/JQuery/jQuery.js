420aa9bfcf911c3317f309cef3d6188574efbb84
sumit2jan <ss9779915666@gmail.com>
2026-02-02 16:38:40 +0530

    feat: Completed JQuery With examples

---
+++ Practice/JQuery/jQuery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  // jQuery methods go here...
+  //$("h1").hide();
+ 
+   $("#btn1").click(function(){
+    $("p").stop(true);
+  })
+//   $("p").mouseenter(function(){
+//   alert("You entered p1!");
+//   });
+}); 
