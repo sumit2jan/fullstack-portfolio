ef6f61aa8272853c3936ae7870ca3a11cfbf5db2
sumit2jan <ss9779915666@gmail.com>
2026-02-02 16:36:59 +0530

    feat: Completed JavaScript Assignment on JQuery

---
+++ Practice/Assgn1/index.js
@@ -0,0 +1,9 @@
+ $("button").eq(0).on("click", function(){
+   $("p").eq(0).toggle();
+});
+ $("button").eq(1).on("click", function(){
+   $("p").eq(1).toggle();
+});
+ $("button").eq(2).on("click", function(){
+   $("p").eq(2).toggle();
+});
