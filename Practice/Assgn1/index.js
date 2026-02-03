e24d4fc648831f38e3a4365c3a2c76157c40a5e5
sumit2jan <ss9779915666@gmail.com>
2026-02-03 15:03:18 +0530

    feat: add Dynamic Code to the Assignment

--- Practice/Assgn1/index.js
+++ Practice/Assgn1/index.js
@@ -1,9 +1,14 @@
- $("button").eq(0).on("click", function(){
-   $("p").eq(0).toggle();
-});
- $("button").eq(1).on("click", function(){
-   $("p").eq(1).toggle();
-});
- $("button").eq(2).on("click", function(){
-   $("p").eq(2).toggle();
-});
+//  $("button").eq(0).on("click", function(){
+//    $("p").eq(0).toggle();
+// });
+//  $("button").eq(1).on("click", function(){
+//    $("p").eq(1).toggle();
+// });
+//  $("button").eq(2).on("click", function(){
+//    $("p").eq(2).toggle();
+// });
+
+    $("#div").on("click", "button", function(){
+        $(this).siblings("p").toggle();
+    });
+
